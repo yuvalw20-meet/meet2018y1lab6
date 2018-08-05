f64cf03a7fcfaa20a371f0d8daf8e4ae154d00f4
yuvalw20-meet <yuvalw20@meet.mit.edu>
2018-08-05 06:59:43 -0400

    Stopping here

---
+++ maths_plots.py
@@ -0,0 +1,10 @@
+import turtle
+
+x = 0
+while x<300:
+    y = x**2/300
+    turtle.goto(x,y)
+    x = x+100
+
+
+turtle.mainloop
---
+++ turtlesshapes.py
@@ -0,0 +1,13 @@
+import turtle
+angle = 360
+numPts = 5
+while (True):
+     turtle.left(angle/numPts)
+     turtle.forward(100)
+     angle = angle + 10
+     
+
+
+     
+turtle.mainloop()
+    
